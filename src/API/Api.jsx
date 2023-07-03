d090da715b294815ec44a8f066d168aa38474c1f
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-07-03 12:25:20 +0200

    Update Api.jsx

--- src/API/Api.jsx
+++ src/API/Api.jsx
@@ -1,19 +1,19 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_KEY = '034daa7694c0247bdfccf293ab74a7ba';
+const API_KEY = "034daa7694c0247bdfccf293ab74a7ba";
 
-const BASE_URL = 'https://api.themoviedb.org';
+const BASE_URL = "https://api.themoviedb.org";
 
 const instance = axios.create({
   baseURL: BASE_URL,
-  headers: { 'Content-Type': 'application/json' },
+  headers: { "Content-Type": "application/json" },
   params: {
     api_key: API_KEY,
-    language: 'pl-PL',
+    language: "pl-PL",
   },
 });
 
-export const getData = url => {
+export const getData = (url) => {
   try {
     return instance.get(`/${url}`);
   } catch (error) {
