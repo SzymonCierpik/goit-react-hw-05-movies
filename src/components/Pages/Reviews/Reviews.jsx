379f01268356b43eb80442aeb89ea86c773b731a
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-07-03 14:41:48 +0200

    Update Reviews.jsx

--- src/components/Pages/Reviews/Reviews.jsx
+++ src/components/Pages/Reviews/Reviews.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { getData } from "../../Api/api";
-
+import { getData } from "../../../Api/Api";
 
 const Reviews = () => {
   const [reviews, setReviews] = useState([]);
