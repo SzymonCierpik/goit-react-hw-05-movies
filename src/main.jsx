4aeda2b345048e3eb7820076168c6d8fddadecb9
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-07-03 14:47:28 +0200

    1

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies">
+    <BrowserRouter basename="/sc-goit-react-hw-05-movies.netlify.app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
