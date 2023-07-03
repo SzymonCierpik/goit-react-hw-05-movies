7dbef0d81805176975500abb3399affbe7b444e0
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-07-03 15:00:29 +0200

    Update HomePage.jsx

--- src/components/Pages/HomePage/HomePage.jsx
+++ src/components/Pages/HomePage/HomePage.jsx
@@ -1,28 +1,39 @@
+import { Link, useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import API_KEY from "../../../Api/Api";
-import { getData } from "../../../Api/Api";
+import { getData } from "Api/Api";
 
-const useMovieDetails = () => {
-  const { movieId } = useParams();
-
-  const MOVIE_DETAILS_URL = `3/movie/${Number(
-    movieId
-  )}?api_key=${API_KEY}&language=en-US`;
-
-  const [details, setDetails] = useState([]);
+const HomePage = () => {
+  const MOVIES_URL = `3/trending/movie/day`;
+  const [movies, setMovies] = useState([]);
+  const location = useLocation();
 
   useEffect(() => {
-    getData(MOVIE_DETAILS_URL)
+    getData(MOVIES_URL)
       .then((response) => {
-        setDetails({ ...response.data });
+        setMovies([...response.data.results]);
       })
       .catch((error) => {
         console.log(error.message);
       });
-  }, [MOVIE_DETAILS_URL]);
+  }, [MOVIES_URL]);
 
-  return [details, setDetails];
+  return (
+    <>
+      <h1>Trending today</h1>
+      <ul>
+        {movies.map((film) => {
+          const { original_title, id } = film;
+          return (
+            <li key={id} className={film}>
+              <Link to={`movies/${id}`} state={{ from: location }}>
+                {original_title}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 };
 
-export default useMovieDetails;
+export default HomePage;
